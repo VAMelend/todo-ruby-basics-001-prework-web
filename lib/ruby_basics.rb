468e05e9d98bf47abd68461793d03cb4fa4c7234
Victoria Melendez <vamelend@gmail.com>
2016-06-28 17:28:55 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,27 @@
 def division(num1, num2)
- 
+num1 / num2
 end
 
-def assign_variable(value)
-  
+def assign_variable(value= "Victoria")
+  value
 end
 
-def argue
-  
+def argue(phrase)
+  phrase
 end
 
-def greeting
-
+def greeting(name, location)
+puts "Hi, my name is #{name} and I am from #{location}."
 end
 
-def return_a_value
-
+def return_a_value(phrase= "Nice")
+phrase
 end
 
-def last_evaluated_value
-
+def last_evaluated_value(phrase="expert")
+  phrase
 end
 
-def pizza_party
-
+def pizza_party(phrase= "cheese")
+phrase
 end
